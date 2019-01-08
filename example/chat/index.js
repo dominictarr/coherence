585946a989da867a078c52bab26f80f24845e2ea
Dominic Tarr <dominic.tarr@gmail.com>
2019-01-09 11:55:31 +1300

    use partial updates, triggered by form

--- example/chat/index.js
+++ example/chat/index.js
@@ -8,10 +8,12 @@ var path      = require('path')
 var cont      = require('cont')
 var Stack     = require('stack')
 
-//chat history in lines
+//chat history in lines in this file.
+//in practice you'd probably have some sort of database.
 var history = []
-fs.readFile('/tmp/coherence-example-chat.txt', 'utf8', function (_, str) {
-  history = (str || '').split('\n').filter(Boolean)
+var filename = '/tmp/coherence-example-chat.txt'
+fs.readFile(filename, 'utf8', function (_, str) {
+  history = (str || '').split('\n').filter(Boolean).map(JSON.parse)
   if(!history.length)
     history.push({
       ts: Date.now(),
@@ -21,7 +23,8 @@ fs.readFile('/tmp/coherence-example-chat.txt', 'utf8', function (_, str) {
   coherence.invalidate('latest', Date.now())
 })
 
-
+//render layout. this wraps every full page.
+//this is the place to include menu items, etc...
 var coherence = Coherence(function (opts, content) {
   return ['html',
     ['head',
@@ -32,10 +35,11 @@ var coherence = Coherence(function (opts, content) {
     ['body', content]
   ]
 })
+//the body of the chat. a list of messages.
+//the special part is `div.latest` which is where future messages get inserted.
 .use('messages', function (opts) {
   var start = opts.start | 0
   var end = opts.end || history.length
-  console.log('messages', opts, end - opts.start, history.slice(opts.start, end).length)
   return [
     ['div.messages']
     .concat(
@@ -49,9 +53,11 @@ var coherence = Coherence(function (opts, content) {
           ]
         })
       ),
+    // Empty partial at the end. when this is invalidated,
+    // new messages will be inserted here.
     // Always has the same id, but when it's loaded
     // it will be replaced div.messages without an id.
-    // so it will only be replaced once
+    // so it will only be replaced once.
     ['div.latest', {
       'data-id': 'latest',
       'data-href':'/messages?start='+end,
@@ -59,6 +65,7 @@ var coherence = Coherence(function (opts, content) {
     }]
   ]
 })
+//wrapper around messages view that includes a form for entering text.
 .use('chat', function (opts, apply) {
   var start = Math.max(opts.start || opts.end - 100, 0)
   var end = opts.end || history.length
@@ -69,7 +76,9 @@ var coherence = Coherence(function (opts, content) {
       method: 'POST',
       //disables suggestions. only care about text input,
       //but that's the only form field.
-      autocomplete: 'off'
+      autocomplete: 'off',
+      'data-invalidate': 'latest',
+      'data-reset': 'true',
     },
       ['input', {type: 'text', name: 'text'}],
       ['button', 'submit']
@@ -87,22 +96,29 @@ http.createServer(Stack(
     if(req.method == 'POST') {
       var body = ''
       req.on('data', function (d) { body += d })
-      req.on('end', function (d) { req.body = QS.parse(body); next() })
+      req.on('end', function (d) { console.log('BODY', body); req.body = QS.parse(body); next() })
     }
     else
       next()
   },
   function (req, res, next) {
     if(req.method == 'POST') {
-      history.push({ts: Date.now(), author: null, text: req.body.text})
-      coherence.invalidate('latest', Date.now())
-      console.log("RETURN EMPTY")
-      return res.end('')
+      var data = {ts: Date.now(), author: null, text: req.body.text}
+      history.push(data)
+      //really, should have a thing here to ensure that only one append happens at a time
+      //but this is just example code so skip it.
+      fs.appendFile(filename, JSON.stringify(data)+  '\n', 'utf8', function (err) {
+        coherence.invalidate('latest', Date.now())
+        //redirect to get so this still works http only app, without javascript
+        //(although you have to reload to get new messages)
+        res.setHeader('location', '/chat')
+        res.statusCode = 303
+        return res.end('')
+      })
     }
-    next()
+    else
+      next()
   },
   coherence
 )).listen(8011)
-
-
 
