980d61a207e4bd96626de9bc12bb399a4c4d4a2f
Dominic Tarr <dominic.tarr@gmail.com>
2019-02-21 22:23:27 +1300

    refactor chat example

---
+++ example/chat/db.js
@@ -0,0 +1,33 @@
+var fs = require('fs')
+
+module.exports = function (filename, cb) {
+  var db, history = []
+  fs.readFile(filename, 'utf8', function (_, str) {
+    history = (str || '').split('\n').filter(Boolean).map(function (e) {
+      //it's theoritically possible that two appends happen
+      //happen at once and that might cause something invalid so
+      //do parse inside of try.
+      try { return JSON.parse(e) }
+      catch (ignore) { }
+    })
+    if(!history.length)
+      history.push({
+        ts: Date.now(),
+        author: 'coherence-bot',
+        text: 'welcome to coherence chat example'
+      })
+
+    cb(null, db)
+  })
+
+  return db = {
+    array: function () { return history },
+    append: function (data, cb) {
+      fs.appendFile(filename, JSON.stringify(data)+  '\n', 'utf8', function (err) {
+        cb()
+      })
+    },
+  }
+}
+
+
--- example/chat/index.js
+++ example/chat/index.js
@@ -1,43 +1,21 @@
-var Coherence = require('../..')
-var http      = require('http')
-var QS        = require('querystring')
-var ago       = require('nice-ago')
-var fs        = require('fs')
-var dir       = process.argv[2] || process.cwd()
-var path      = require('path')
-var cont      = require('cont')
-var Stack     = require('stack')
+var Coherence  = require('../..')
+var http       = require('http')
+var QS         = require('querystring')
+var ago        = require('nice-ago')
+var dir        = process.argv[2] || process.cwd()
+var path       = require('path')
+var cont       = require('cont')
+var Stack      = require('stack')
+var BodyParser = require('urlencoded-request-parser')
+var Static     = require('ecstatic')
 
 //chat history in lines in this file.
 //in practice you'd probably have some sort of database.
-var history = []
-var filename = '/tmp/coherence-example-chat.txt'
-function append(data, cb) {
-  history.push(data)
-  //really, should have a thing here to ensure that only one append happens at a time
-  //but this is just example code so skip it.
-  fs.appendFile(filename, JSON.stringify(data)+  '\n', 'utf8', function (err) {
+var db = require('./db')(
+  '/tmp/coherence-example-chat.txt',
+  function () {
     coherence.invalidate('latest', Date.now())
-    cb()
   })
-}
-
-fs.readFile(filename, 'utf8', function (_, str) {
-  history = (str || '').split('\n').filter(Boolean).map(function (e) {
-    //it's theoritically possible that two appends happen
-    //happen at once and that might cause something invalid so
-    //do parse inside of try.
-    try { return JSON.parse(e) }
-    catch (ignore) { }
-  })
-  if(!history.length)
-    history.push({
-      ts: Date.now(),
-      author: 'coherence-bot',
-      text: 'welcome to coherence chat example'
-    })
-  coherence.invalidate('latest', Date.now())
-})
 
 //render layout. this wraps every full page.
 //this is the place to include menu items, etc...
@@ -66,7 +44,7 @@ var coherence = Coherence(function (opts, content) {
   return [
     ['div.messages']
     .concat(
-        history
+        db.array()
         .slice(opts.start, end)
         .map(function (e) {
           var date = new Date(e.ts)
@@ -95,7 +73,7 @@ var coherence = Coherence(function (opts, content) {
 //wrapper around messages view that includes a form for entering text.
 .use('chat', function (opts, apply) {
   var start = Math.max(opts.start || opts.end - 100, 0)
-  var end = opts.end || history.length
+  var end = opts.end || db.array().length
   return [
     'div.page',
     ['div.chat', apply('messages', {start: start, end: end})],
@@ -131,25 +109,13 @@ http.createServer(Stack(
     req.context = QS.parse(req.headers.cookie||'') || {}
     next()
   },
+  Static({
+    root: path.join(__dirname, 'static'),
+    baseDir: '/static'
+  }),
+  BodyParser(),
   function (req, res, next) {
-    if(/^\/static\//.test(req.url))
-      fs.createReadStream(__dirname+req.url).pipe(res)
-    else next()
-  },
-  function (req, res, next) {
-    if(req.method == 'POST') {
-      var body = ''
-      req.on('data', function (d) { body += d })
-      req.on('end', function (d) {
-        console.log('BODY', body)
-        req.body = QS.parse(body)
-        next()
-      })
-    }
-    else
-      next()
-  },
-  function (req, res, next) {
+    console.log('BODY', req.body)
     function redirect () {
       //redirect to get so this still works http only app, without javascript
       //(although you have to reload to get new messages)
@@ -164,11 +130,13 @@ http.createServer(Stack(
         redirect()
       }
       else {
-        append({
-          ts: Date.now(),
-          author: req.context.name,
-          text: req.body.text
-        }, redirect)
+        var ts = Date.now()
+        db.append({
+          ts: ts, author: req.context.name, text: req.body.text
+        }, function () {
+          coherence.invalidate('latest', ts)
+          redirect()
+        })
       }
     }
     else
@@ -176,4 +144,5 @@ http.createServer(Stack(
   },
   coherence
 )).listen(8011)
+
 
